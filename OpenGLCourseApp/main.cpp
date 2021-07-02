89677f239f1b6bcc44b32052b4356cdbd51440fa
liviusgrosu <livius.grosu@ryerson.ca>
2021-07-02 16:26:52 -0300

    Initial opengl commit

---
+++ OpenGLCourseApp/main.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main() {
+    if (!glfwInit()) {
+        // GLFW init failed
+        printf("GLFW init failed...");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Don't want to use open depricated functions
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    if (!mainWindow) {
+        printf("GLFW window failed to create...");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Get buffer size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // Allow modern extensions features
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        printf("GLEW init failed...");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+    
+    while (!glfwWindowShouldClose(mainWindow)) {
+        glfwPollEvents();
+        
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
+    return 0;
+}
