fba3b1e8f54984c1ff1aadb3d727b59d2d044b01
LiviusG <liviusg@nauticsgroup.com>
2021-08-06 10:00:54 -0300

    Implemented a skybox

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -25,6 +25,8 @@
 #include "Material.h"
 #include "Model.h"
 
+#include "Skybox.h"
+
 const float toRadians = 3.14159265f / 180.0f;
 
 
@@ -51,6 +53,8 @@ GLfloat uh60Angle = 0.0f;
 DirectionalLight mainLight;
 PointLight pointLights[MAX_POINT_LIGHTS];
 SpotLight spotLights[MAX_SPOT_LIGHTS];
+
+Skybox skybox;
 
 unsigned int pointLightCount = 0;
 unsigned int spotLightCount = 0;
@@ -254,6 +258,14 @@ void OmniShadowMapPass(PointLight* light)
 }
 
 void RenderPass(glm::mat4 projectionMatrix, glm::mat4 viewMatrix) {
+	glViewport(0, 0, 1366, 768);
+
+	// Clear the screen
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	skybox.DrawSkybox(viewMatrix, projectionMatrix);
+	
 	shaderList[0].UseShader();
 
 	uniformModel = shaderList[0].GetModelLocation();
@@ -263,12 +275,6 @@ void RenderPass(glm::mat4 projectionMatrix, glm::mat4 viewMatrix) {
 	uniformCameraPosition = shaderList[0].GetCameraPositionLocation();
 	uniformSpecularIntensity = shaderList[0].GetSpecularIntensityLocation();
 	uniformShininess = shaderList[0].GetShininessLocation();
-
-	glViewport(0, 0, 1366, 768);
-
-	// Clear the screen
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
 	glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(viewMatrix));
@@ -359,6 +365,16 @@ int main()
 		1.0f, 0.0f, 0.0f,
 		20.0f);
 	spotLightCount++;
+
+	std::vector<std::string> skyboxFaces;
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_lf.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_up.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_dn.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_bk.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_ft.tga");
+
+	skybox = Skybox(skyboxFaces);
 
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformCameraPosition = 0, uniformSpecularIntensity = 0, uniformShininess = 0;
 	
